15d91df7d9fd25ae6477a7d3ee3a0a2394b33da3
Collins njau <collinsnjau39@gmail.com>
2018-01-26 00:28:28 +0300

    I have improved the method to get the index of the passed argument

--- position.js
+++ position.js
@@ -1,13 +1,14 @@
 // Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. The returned value should be a number.
 function getIndexToIns(arr,num) {
   // Find my place in this sorted array.
-  var newArray=[num]
+  var newArray=[]
   for(var i=0;i<arr.length;i++){
-    newArray.push(arr[i])
+    if(arr[i]<num){
+      newArray.push(arr[i])
+    }
   }
-  var final=newArray.sort()
-  var number=final.indexOf(num)
-  return number;
+
+  return newArray.length;
 }
 
-getIndexToIns([40, 60,10],50);
+getIndexToIns([2, 20, 10], 19);
